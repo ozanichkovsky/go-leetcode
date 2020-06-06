9395e2d4a3376be4bf0a5d12bb3095920fbd0b47
Oleksandr Zanichkovskyi <eternity.lviv@gmail.com>
2020-06-06 23:30:42 +0300

    Two Sum - added less efficient implementations

---
+++ p1twosum/README.md
@@ -0,0 +1,5 @@
+## Solution of "Two Sum" problem on [leetcode.com](http://leetcode.com)
+[Two Sum](https://leetcode.com/problems/two-sum/)
+
+Contains 3 different solutions from the less efficient to the most efficient.
+
--- p1twosum/twoSum.go
+++ p1twosum/twoSum.go
@@ -1,6 +1,36 @@
 package p1twosum
 
 func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == target - nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	hash := make(map[int]int)
+
+	for i, n := range nums {
+		hash[n] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+
+		if v, ok := hash[complement]; ok && v != i {
+			return []int{i, v}
+		}
+	}
+
+	return nil
+}
+
+func twoSum3(nums []int, target int) []int {
 	hash := make(map[int]int)
 
 	for i := 0; i < len(nums); i++ {
